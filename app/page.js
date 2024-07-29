a7bc1bd4ab49a3db34aadfe538b92a325307785e
David Paimon <davidfelipe.fj@gmail.com>
2024-07-28 23:55:15 -0500

    facedetected

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
-        Alarm detector
+        Alarm detector (app para telefono celular)
       </h1>
       <ObjectDetection />
     </main>
